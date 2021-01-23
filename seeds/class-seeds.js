86bc08b53b55c5bc90bd2e654b0576902cd3f68e
amiedawn <amie.d.carroll@gmail.com>
2021-01-23 12:59:01 -0700

    revise class seeds to add extra categories

--- seeds/class-seeds.js
+++ seeds/class-seeds.js
@@ -89,6 +89,90 @@ const classData = [
     class_name: "60 minute Pilates",
     category_id: 4,
   },
+  {
+    class_name: "30 minute Pump",
+    category_id: 3,
+  },
+  {
+    class_name: "30 minute Pump",
+    category_id: 4,
+  },
+  {
+    class_name: "30 minute Circuit",
+    category_id: 2,
+  },
+  {
+    class_name: "30 minute Circuit",
+    category_id: 3,
+  },
+  {
+    class_name: "30 minute Step Aerobics",
+    category_id: 4,
+  },
+  {
+    class_name: "30 minute Combat",
+    category_id: 2,
+  },
+  {
+    class_name: "30 minute Worx",
+    category_id: 4,
+  },
+  {
+    class_name: "30 minute Barre",
+    category_id: 4,
+  },
+  {
+    class_name: "30 minute Yoga",
+    category_id: 3,
+  },
+  {
+    class_name: "30 minute Pilates",
+    category_id: 3,
+  },
+  {
+    class_name: "60 minute Pump",
+    category_id: 3,
+  },
+  {
+    class_name: "60 minute Pump",
+    category_id: 4,
+  },
+  {
+    class_name: "60 minute Circuit",
+    category_id: 2,
+  },
+  {
+    class_name: "60 minute Circuit",
+    category_id: 3,
+  },
+  {
+    class_name: "60 minute Step",
+    category_id: 4,
+  },
+  {
+    class_name: "60 minute Spin",
+    category_id: 2,
+  },
+  {
+    class_name: "60 minute Combat",
+    category_id: 2,
+  },
+  {
+    class_name: "60 minute Worx",
+    category_id: 4,
+  },
+  {
+    class_name: "60 minute Barre",
+    category_id: 4,
+  },
+  {
+    class_name: "60 minute Yoga",
+    category_id: 3,
+  },
+  {
+    class_name: "60 minute Pilates",
+    category_id: 3,
+  },
 ];
 
 const seedClasses = () => Class.bulkCreate(classData);
