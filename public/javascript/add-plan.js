a6ad8d12a9bc30e1b8d68686c6d7e8bd68f654c1
AnnMarkham <ann.e.markham@outlook.com>
2021-01-23 12:45:07 -0700

    dashboard and create plan forms same now

--- public/javascript/add-plan.js
+++ public/javascript/add-plan.js
@@ -1,2 +1,27 @@
-
+async function newFormHandler(event) {
+    event.preventDefault();
   
+    const plan_title = document.querySelector('input[name="plan-title"]').value;
+    const category_name = document.querySelector('input[name="category-name"]').value;
+    const class_name = document.querySelector('input[name="class-name"]').value;
+  
+    const response = await fetch(`/api/plans`, {
+      method: 'POST',
+      body: JSON.stringify({
+        plan_title,
+        category_name,
+        class_name,
+      }),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+  
+    if (response.ok) {
+      document.location.replace('/dashboard');
+    } else {
+      alert(response.statusText);
+    }
+  }
+  
+  document.querySelector('.new-plan-form').addEventListener('submit', newFormHandler);  
