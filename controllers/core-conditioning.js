28b1adde72f82ee2b16d0334144dff5af4805e41
AnnMarkham <ann.e.markham@outlook.com>
2021-01-25 13:26:24 -0700

    class/category working for core conditioning

--- controllers/core-conditioning.js
+++ controllers/core-conditioning.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const sequelize = require("../config/connection");
-const { Class } = require('../models');
+const { Class, Category } = require('../models');
 //const withAuth = require('../utils/auth');
 
 /* get all plans for dashboard ADD WHERE USER ID & REQ.SESSION after login works
@@ -9,7 +9,15 @@ router.get('/', (req, res) => {
   console.log("================ get dashboard route");
   Class.findAll({
     attributes: ["id", "class_name"],
-    })
+    include:[
+      {
+        model: Category,
+        where: {
+          id: 3
+        },
+      }
+    ]
+    })  
     .then((dbClassData) => {
       console.log("make it to render dashboard");
       const classes = dbClassData.map((classes) => classes.get({ plain: true }));
