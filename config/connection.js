d9c5198839ef7fdd679df50ee328c7e07dba761e
Your Name <alanmayer@email.arizona.edu>
2021-01-25 18:15:44 -0800

    login 2.0

--- config/connection.js
+++ config/connection.js
@@ -9,9 +9,9 @@ if (process.env.JAWSDB_URL) {
   sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
   sequelize = new Sequelize(
-    "workout_planner_db",
-    "root",
-    "A1251370033a$", {
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PW, {
       host: "localhost",
       dialect: "mysql",
       port: 3306,
