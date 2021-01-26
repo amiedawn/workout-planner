0e64ed7d8891674c6ab7e9dacb5dd78f87d0e8ee
Your Name <alanmayer@email.arizona.edu>
2021-01-25 21:36:16 -0800

    root

--- config/connection.js
+++ config/connection.js
@@ -1,21 +1,36 @@
 // import the Sequelize constructor from the library
 
-const Sequelize = require("sequelize");
-require("dotenv").config();
+// const Sequelize = require("sequelize");
+// require("dotenv").config();
 
-let sequelize;
+// let sequelize;
 
-if (process.env.JAWSDB_URL) {
-  sequelize = new Sequelize(process.env.JAWSDB_URL);
-} else {
-  sequelize = new Sequelize(
-    "workout_planner_db",
-    "root",
-    "A1251370033a$", {
-      host: "localhost",
-      dialect: "mysql",
-      port: 3306,
-    }
-  );
-}
+// if (process.env.JAWSDB_URL) {
+//   sequelize = new Sequelize(process.env.JAWSDB_URL);
+// } else {
+//   sequelize = new Sequelize(
+//     "workout_planner_db",
+//     "root",
+//     "A1251370033a$", {
+//       host: "localhost",
+//       dialect: "mysql",
+//       port: 3306,
+//     }
+//   );
+// }
+// module.exports = sequelize;
+
+const Sequelize = require('sequelize');
+
+require('dotenv').config();
+
+// create connection to our db
+const sequelize = process.env.JAWSDB_URL
+  ? new Sequelize(process.env.JAWSDB_URL)
+  : new Sequelize("workout_planner_db", "", "A1251370033a$", {
+      host: 'localhost',
+      dialect: 'mysql',
+      port: 3306
+    });
+
 module.exports = sequelize;
