b9200c8d1c3b88156ad35be50dc10c7d1fff0840
amiedawn <amie.d.carroll@gmail.com>
2021-01-26 10:09:23 -0700

    modify connection file and delete lib folder

--- config/connection.js
+++ config/connection.js
@@ -1,15 +1,24 @@
+const Sequelize = require("sequelize");
 
-const Sequelize = require('sequelize');
-
-require('dotenv').config();
+require("dotenv").config();
 
 // create connection to our db
-const sequelize = process.env.JAWSDB_URL
-  ? new Sequelize(process.env.JAWSDB_URL)
-  : new Sequelize("workout_planner_db", "", "A1251370033a$", {
-      host: 'localhost',
-      dialect: 'mysql',
-      port: 3306
-    });
+let sequelize;
+
+if (process.env.JAWSDB_URL) {
+  console.log(process.env.JAWSDB_URL);
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+  sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PW,
+    {
+      host: "localhost",
+      dialect: "mysql",
+      port: 3306,
+    }
+  );
+}
 
 module.exports = sequelize;
