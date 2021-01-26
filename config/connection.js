1fd46cba37b032240d1fc69edaeec71de138cb24
AnnMarkham <ann.e.markham@outlook.com>
2021-01-26 12:14:49 -0700

    checkboxes working

--- config/connection.js
+++ config/connection.js
@@ -1,15 +1,18 @@
 
 const Sequelize = require('sequelize');
-
 require('dotenv').config();
+//add jawsdb and add it in heroku too
+let sequelize;
 
-// create connection to our db
-const sequelize = process.env.JAWSDB_URL
-  ? new Sequelize(process.env.JAWSDB_URL)
-  : new Sequelize("workout_planner_db", "", "A1251370033a$", {
-      host: 'localhost',
-      dialect: 'mysql',
-      port: 3306
+if (process.env.JAWSDB_URL) {
+    sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+    sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+        host: 'localhost',
+        dialect: 'mysql',
+        port: 3306
     });
+}
+
 
 module.exports = sequelize;
