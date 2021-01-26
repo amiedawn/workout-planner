4d1c60e9778baec7271f2655f6a521eb0424a7fc
Your Name <alanmayer@email.arizona.edu>
2021-01-25 21:37:34 -0800

    root 2.0

--- config/connection.js
+++ config/connection.js
@@ -1,24 +1,3 @@
-// import the Sequelize constructor from the library
-
-// const Sequelize = require("sequelize");
-// require("dotenv").config();
-
-// let sequelize;
-
-// if (process.env.JAWSDB_URL) {
-//   sequelize = new Sequelize(process.env.JAWSDB_URL);
-// } else {
-//   sequelize = new Sequelize(
-//     "workout_planner_db",
-//     "root",
-//     "A1251370033a$", {
-//       host: "localhost",
-//       dialect: "mysql",
-//       port: 3306,
-//     }
-//   );
-// }
-// module.exports = sequelize;
 
 const Sequelize = require('sequelize');
 
